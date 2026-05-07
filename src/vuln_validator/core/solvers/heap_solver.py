572fd2f10010a9228e0a2754d422816ad3f678f4
Marko <marko.buehler@students.fhnw.ch>
2026-05-07 17:59:12 +0200

    refactor: put hooks in separate file

--- src/vuln_validator/core/solvers/heap_solver.py
+++ src/vuln_validator/core/solvers/heap_solver.py
@@ -1,39 +1,8 @@
 from .base_memory_solver import BaseMemorySolver
-import claripy
+from .hooks.heap_hooks import MyFakeMalloc
 import logging
-import angr
 
 logger = logging.getLogger(__name__)
-
-
-class MyFakeMalloc(angr.SimProcedure):
-    HEAP_START = claripy.BVV(0x600000, 64)
-
-    def run(self, size):
-        if "heap_ptr" not in self.state.globals:
-            self.state.globals["heap_ptr"] = self.HEAP_START
-            self.state.globals["heap_canary_list"] = []
-
-        ret_addr = self.state.globals["heap_ptr"]
-
-        canary_addr = ret_addr + size
-        self.state.memory.store(
-            canary_addr,
-            claripy.BVV(0xDEADBEEF, 32),
-            endness=self.state.arch.memory_endness,
-        )
-        self.state.globals["heap_canary_list"].append(
-            {
-                "addr": self.state.solver.eval(canary_addr),
-                "expected": 0xDEADBEEF,
-                "padding_size": 4,  # Default für Heap Canary
-                "source": "malloc_hook",
-            }
-        )
-
-        self.state.globals["heap_ptr"] = canary_addr + 0x4
-
-        return ret_addr
 
 
 class HeapOverflowSolver(BaseMemorySolver):
---
+++ src/vuln_validator/core/solvers/hooks/heap_hooks.py
@@ -0,0 +1,32 @@
+import angr
+import claripy
+
+
+class MyFakeMalloc(angr.SimProcedure):
+    HEAP_START = claripy.BVV(0x600000, 64)
+
+    def run(self, size):
+        if "heap_ptr" not in self.state.globals:
+            self.state.globals["heap_ptr"] = self.HEAP_START
+            self.state.globals["heap_canary_list"] = []
+
+        ret_addr = self.state.globals["heap_ptr"]
+
+        canary_addr = ret_addr + size
+        self.state.memory.store(
+            canary_addr,
+            claripy.BVV(0xDEADBEEF, 32),
+            endness=self.state.arch.memory_endness,
+        )
+        self.state.globals["heap_canary_list"].append(
+            {
+                "addr": self.state.solver.eval(canary_addr),
+                "expected": 0xDEADBEEF,
+                "padding_size": 4,  # Default für Heap Canary
+                "source": "malloc_hook",
+            }
+        )
+
+        self.state.globals["heap_ptr"] = canary_addr + 0x4
+
+        return ret_addr

8eeb2bf602900eae14e261b5bfc5230fb2429a41
Marko <marko.buehler@students.fhnw.ch>
2026-07-15 10:52:44 +0200

    chore: add uaf binaries

---
+++ tests/fixtures/use_after_free/uaf_simpel.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    void (*func_ptr)(void);
+    char data[32];
+} Victim;
+
+void safe_function() {
+    printf("Safe function called.\n");
+}
+
+void hacked_function() {
+    printf("VULNERABILITY EXPLOITED: Hacked function called!\n");
+}
+
+int main() {
+    Victim *v = (Victim *)malloc(sizeof(Victim));
+    v->func_ptr = safe_function;
+    strcpy(v->data, "Original Data");
+
+    free(v); 
+
+    Victim *fake = (Victim *)malloc(sizeof(Victim));
+    fake->func_ptr = hacked_function; 
+    strcpy(fake->data, "Attacker Data");
+
+    printf("Triggering UAF...\n");
+    if (v != NULL) {
+        v->func_ptr(); 
+    }
+
+    return 0;
+}
